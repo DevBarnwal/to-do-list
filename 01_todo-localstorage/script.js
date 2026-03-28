3e8ca87b86fd9ee8bb66dde8f3bc87a7d94b0d21
DevBarnwal <dev.bharat.03@gmail.com>
2026-03-28 17:32:34 +0530

    to-do-app done

--- 01_todo-localstorage/script.js
+++ 01_todo-localstorage/script.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const taskinput = document.getElementById('todo-input');
   const todolist = document.getElementById('todo-list');
 
-  let tasks = JSON.parse(localStorage.setItem) || [];
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+  tasks.forEach(task => {
+    renderTasks(task);
+  });
 
   inputButton.addEventListener('click', function(){
     const tasktext = taskinput.value;
@@ -15,9 +19,33 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     tasks.push(newTask);
     saveTasks();
+    renderTasks(newTask);
     taskinput.value = "";
     console.log(tasks);
   })
+
+  function renderTasks(task){
+    const li = document.createElement('li');
+    li.setAttribute("data-id", task.uniqueId);
+    li.innerHTML = `<span>${task.text}</span>
+    <button>Delete</button>`;
+    todolist.appendChild(li);
+
+    li.addEventListener('click',(e)=>{
+      if(e.target.tagName === 'BUTTON') return;
+      task.completed = !task.completed;
+      li.classList.toggle('completed');
+      saveTasks();
+    });
+
+    li.querySelector('button').addEventListener('click', (e) => {
+      e.stopPropagation();
+      tasks = tasks.filter(t => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    })
+
+  }
 
   function saveTasks(){
     localStorage.setItem('tasks', JSON.stringify(tasks));
